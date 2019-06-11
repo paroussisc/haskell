79f908b174f96a0aa447010e29a6b6337a1eaaba
Christopher Paroussis <christopher.paroussis@gmail.com>
2019-06-11 18:29:57 +0100

    Little speedup.

--- 99_questions/src/FourthTen.hs
+++ 99_questions/src/FourthTen.hs
@@ -2,5 +2,6 @@ module FourthTen  where
 
   -- Question 31
   isPrime :: (Eq a, Integral a) => a -> Bool
-  isPrime n     = gd == 2
-      where gd = length [y | y <- [1..n], n `rem` y == 0]
+  isPrime n     = gd == 1
+      where  m = n `div` 2
+             gd = length [y | y <- [1..m], n `rem` y == 0]
