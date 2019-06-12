e361bb0933c6a974172e37fff04de4320aa42831
Christopher Paroussis <christopher.paroussis@gmail.com>
2019-06-12 11:18:12 +0100

    Up to 41 done.

--- 99_questions/src/FourthTen.hs
+++ 99_questions/src/FourthTen.hs
@@ -1,7 +1,54 @@
 module FourthTen  where
 
-  -- Question 31
-  isPrime :: (Eq a, Integral a) => a -> Bool
-  isPrime n     = gd == 1
-      where  m = n `div` 2
-             gd = length [y | y <- [1..m], n `rem` y == 0]
+import           Data.List (groupBy)
+
+-- Question 31
+isPrime :: (Eq a, Integral a) => a -> Bool
+isPrime n     = gd == 1
+    where  m = n `div` 2
+           gd = length [y | y <- [1..m], n `rem` y == 0]
+
+-- Question 32
+gcd' :: (Eq a, Integral a) => a -> a -> a
+gcd' 0 y     =  y
+gcd' x y     =  helper (y `mod` x) x
+    where  helper x y | x < 0  = helper (-x) y
+                      | y < 0  = helper x (-y)
+                      | x < y = gcd' x y
+                      | otherwise = gcd' y x
+
+-- Question 33
+coprime :: (Eq a, Integral a) => a -> a -> Bool
+coprime x y     =  (gcd' x y) == 1
+
+-- Question 34
+totient :: Int -> Int
+totient   n  =  length $ filter (coprime n) [1..n]
+
+-- Question 35
+primeFactors :: Int -> [Int]
+primeFactors   n  =  primeFactors' n 2
+  where
+    primeFactors' n f
+      | f*f > n        = [n]
+      | n `mod` f == 0 = f : primeFactors' (n `div` f) f
+      | otherwise      = primeFactors' n (f + 1)
+
+-- Question 36
+primeFactorsMult :: Int -> [(Int, Int)]
+primeFactorsMult   n  =   map (pairUp) $ groupBy (==)  $ primeFactors n
+    where pairUp x = (head x, length x)
+
+-- Question 37
+totientImproved :: Int -> Int
+totientImproved   1 = 1
+totientImproved   n =  product [(p - 1)*p^(m-1) | (p,m) <- primeFactorsMult n]
+
+-- Question 40
+goldbach :: Int -> (Int, Int)
+goldbach   n = head [(x, y) | x <- primes, y <- primes, x + y == n]
+    where primes = filter (isPrime) [1..n]
+
+-- Question 40
+goldbachList :: Int -> Int -> [(Int, Int)]
+goldbachList   l u = map FourthTen.goldbach [x | x <- [l..u], x `mod` 2 == 0]
--- 99_questions/test/FourthTenSpec.hs
+++ 99_questions/test/FourthTenSpec.hs
@@ -6,5 +6,47 @@ import           Test.Hspec
 spec :: Spec
 spec = do
   describe "Question 31" $ do
-    it "Insert at..." $ do
+    it "Prime checker..." $ do
       (isPrime 7) == (True)
+
+  describe "Question 32" $ do
+    it "Greatest common divisor..." $ do
+      (gcd' 252 105) == (21)
+
+  describe "Question 33" $ do
+    it "Coprime..." $ do
+      (coprime 35 64) == (True)
+    it "Not coprime..." $ do
+      (coprime 35 20) == (False)
+
+  describe "Question 34" $ do
+    it "Totient..." $ do
+      (totient 10) == (4)
+    it "Totient..." $ do
+      (totient 523) == (522)
+    it "Totient..." $ do
+      (totient 1) == (1)
+
+  describe "Question 35" $ do
+    it "Prime factors..." $ do
+      (primeFactors 315) == ([3, 3, 5, 7])
+
+  describe "Question 36" $ do
+    it "Prime factors & multiplicity..." $ do
+      (primeFactorsMult 315) == ([(3,2),(5,1),(7,1)])
+
+  describe "Question 37" $ do
+    it "Totient..." $ do
+      (totientImproved 10) == (4)
+    it "Totient..." $ do
+      (totientImproved 523) == (522)
+    it "Totient..." $ do
+      (totientImproved 1) == (1)
+
+    describe "Question 40" $ do
+      it "Goldbach..." $ do
+        (goldbach 28) == ((5,23))
+
+      describe "Question 41" $ do
+        it "Goldbach list..." $ do
+          (goldbachList 9 20) == ([(3,7),(5,7),(3,11),(3,13),(5,13),(3,17)])
