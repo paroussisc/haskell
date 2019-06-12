f2d18cee1481e26e07322bfeaf84878f0642b7d9
Christopher Paroussis <christopher.paroussis@gmail.com>
2019-06-12 14:35:57 +0100

    Gray codes...

--- 99_questions/src/LogicCodes.hs
+++ 99_questions/src/LogicCodes.hs
@@ -1,5 +1,6 @@
 module LogicCodes  where
 
+-- Question 46
 and' :: Bool -> Bool -> Bool
 and' True True = True
 and' _    _    = False
@@ -27,3 +28,14 @@ xor' _ _        = False
 table :: (Bool -> Bool -> Bool) -> IO ()
 table f = mapM_ putStrLn [show x ++ " " ++ show y ++ " " ++ show (f x y)
                           | x <-  [True, False], y <-  [True, False]]
+
+-- Skipping questions 47-48 since they're really meant for other languages
+-- / I cba.
+
+-- Question 49
+gray :: (Num a, Eq a) => a -> [String]
+gray 1 = ["0", "1"]
+gray n = map ('0' :) prev ++ map ('1' :) (reverse prev)
+    where prev = gray (n-1)
+
+--  Skipping 50 for now
